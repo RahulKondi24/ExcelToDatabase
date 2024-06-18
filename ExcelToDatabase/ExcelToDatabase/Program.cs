be48c7d473f983a1753e511e9fc2934112a6cfa5
Rahul Kondi <rahulkondi24@gmail.com>
2024-06-18 13:27:37 +0530

    added con and excel file path

--- ExcelToDatabase/ExcelToDatabase/Program.cs
+++ ExcelToDatabase/ExcelToDatabase/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Starting application...");
+
+// Database connection string
+string connectionString = "Server=LAPTOP-46NPMGS0\\SQLEXPRESS;Database=ATSDB;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=true";
+
+string excelFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ExcelFile", "Salary Sheet Sample.xlsx");
