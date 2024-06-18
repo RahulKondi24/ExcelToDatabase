82c96cd2eb95a44e1a38d8d00b8189fa7b72f861
Rahul Kondi <rahulkondi24@gmail.com>
2024-06-18 13:36:32 +0530

    added  two nuget packages and added two services one is reads the excel file and onther one is insert the data into database.

--- ExcelToDatabase/ExcelToDatabase/Program.cs
+++ ExcelToDatabase/ExcelToDatabase/Program.cs
@@ -3,4 +3,13 @@
 // Database connection string
 string connectionString = "Server=LAPTOP-46NPMGS0\\SQLEXPRESS;Database=ATSDB;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=true";
 
+// Construct the relative path to the Excel file
 string excelFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ExcelFile", "Salary Sheet Sample.xlsx");
+Console.WriteLine($"Excel file path: {excelFilePath}");
+
+// Ensure the file path is correct
+if (!File.Exists(excelFilePath))
+{
+    Console.WriteLine($"The specified Excel file does not exist at: {Path.GetFullPath(excelFilePath)}");
+    return;
+}
